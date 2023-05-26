b99c0d5c3614f8c75d004e3782eaeef65c81a81c
Dhony Mark Mendoza <19102710@usc.edu.ph>
2023-05-26 18:09:55 +0800

    register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Login from "./components/Login";
+import Register from "./components/Register";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
     </div>
   );
 }
